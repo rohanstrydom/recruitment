43c7c7ef62762080105a54c66ff0d87ec974d1a3
Rohan Strydom <rohan.strydom@gmail.com>
2018-04-23 19:23:59 +0200

    API's updated

--- functions/candidate_save_phone_number.php
+++ functions/candidate_save_phone_number.php
@@ -5,16 +5,13 @@ function candidate_save_phone_number(){
 
     $phone_number = $_POST['phone_number'];
     $position_code = $_POST['position_code'];
+
     //$phone_number = '0823749048';
     //$position_code = '0DIGB0E020180413';
     
     $position_object = get_page_by_title( $position_code, OBJECT, 'position' );
-    
-    //print_r($position_object);
 
     $position_name = get_field('position_name', $position_object->ID);
-
-    //echo $position_name;
 
     // check if post already exist
     $candidate = get_page_by_title( $phone_number, OBJECT, 'candidate' );
@@ -43,8 +40,6 @@ function candidate_save_phone_number(){
         $candidate = wp_insert_post( $candidate_post, $wp_error = true );
 
         update_field('phone_number',$phone_number, $candidate);
-        
-        print_r($candidate);
 
         add_position_to_candidate($candidate, $position_code, $position_name);
 
@@ -57,8 +52,6 @@ function candidate_save_phone_number(){
 function add_position_to_candidate($candidate_id, $position_code, $position_name){
 
     $position = get_page_by_title( $position_code, OBJECT, 'position' );
-
-    print_r($position);
 
     $date_time = new DateTime();
     $date_time->format('Y-m-d H:i:s');
